f5c85d436bfc756713723771f20f945f126ff0b3
vcf-asa-team <lennonc@gmail.com>
2024-12-27 06:40:32 -0600

    add json

--- backend/open_webui/retrieval/utils.py
+++ backend/open_webui/retrieval/utils.py
@@ -5,6 +5,7 @@ from typing import Optional, Union
 
 import asyncio
 import requests
+import _json
 
 from huggingface_hub import snapshot_download
 from langchain.retrievers import ContextualCompressionRetriever, EnsembleRetriever
